4e17fd434abf119970487bad1077579d9ab57b40
Anders Hoff <inconvergent@gmail.com>
2016-10-29 00:45:31 +0200

    smooth draw

--- src/draw.js
+++ src/draw.js
@@ -1,6 +1,10 @@
 
 import { printInfo, printWarn } from './utils';
 
+const PI = Math.PI;
+const TWOPI = Math.PI * 2.0;
+const HPI = Math.PI * 0.5;
+
 export function drawPath(ctx, p) {
   ctx.beginPath();
   ctx.moveTo(p[0].x, p[0].y);
@@ -10,3 +14,17 @@ export function drawPath(ctx, p) {
   ctx.stroke();
 }
 
+export function drawPathDots(ctx, p, rad) {
+  for (let i = 1; i < p.length; i++) {
+    ctx.beginPath();
+    ctx.arc(p[i].x, p[i].y, rad, 0, TWOPI);
+    ctx.fill();
+  }
+  drawPath(ctx, p);
+}
+
+export function clear(ctx, width, height) {
+  ctx.beginPath();
+  ctx.rect(0, 0, width, height);
+  ctx.fill();
+}
--- src/scenes.js
+++ src/scenes.js
@@ -2,39 +2,66 @@
 
 import { getLinspaceYLinspaceX, getRndYLinspaceX, permuteY } from './random';
 import { printInfo, printWarn } from './utils';
-import { drawPath } from './draw';
+import { drawPath, drawPathDots, clear} from './draw';
+
+const PI = Math.PI;
+const TWOPI = Math.PI * 2.0;
+const HPI = Math.PI * 0.5;
 
 const WHITE = 'rgba(255, 255, 255, 1.0)';
 const BLACK = 'rgba(0, 0, 0, 1.0)';
-const GRAY = 'rgba(0, 0, 0, 0.7)';
+const GRAY = 'rgba(0, 0, 0, 0.4)';
 const LINEWIDTH = 3;
 
 
 export function getScene1(ctx, width, height) {
   printInfo('making scene 1');
-  const narative = 'Random numbers';
+  const narative = 'Here are some Nodes. But they have no <a href="#2">memory</a>';
 
   ctx.strokeStyle = GRAY;
-  ctx.fillStyle = WHITE;
+  ctx.fillStyle = GRAY;
   ctx.lineWidth = LINEWIDTH;
 
-  const edgeX = width*20/100;
+  const edgeX = width*5/100;
   const edgeY = height*20/100;
 
   const xMin = edgeX;
   const xMax = width - edgeX;
   const yMin = edgeY;
   const yMax = height - edgeY;
+  const yMid = (yMax+yMin)*0.5;
 
-  const num = width / 10;
+  const num = Math.floor(width / 10);
+  const dotSize = 3;
+
+  const frames = 30;
+
+  let path1 = getLinspaceYLinspaceX(num, xMin, xMax, yMid, yMid);
+  let path2 = getRndYLinspaceX(num, xMin, xMax, yMin, yMax);
+  let itt = 0;
 
   function scene() {
+    itt += 1;
     printInfo('scene1');
-    ctx.beginPath();
-    ctx.rect(0, 0, width, height);
-    ctx.fill();
-    const path = getRndYLinspaceX(num, xMin, xMax, yMin, yMax);
-    drawPath(ctx, path);
+    ctx.fillStyle = WHITE;
+    clear(ctx, width, height);
+    ctx.fillStyle = GRAY;
+
+    if (itt%frames===0) {
+      path1 = path2;
+      path2 = getRndYLinspaceX(num, xMin, xMax, yMin, yMax);
+    }
+
+    const path = [];
+    for (let i = 0; i < num; i++) {
+      const y1 = path1[i].y;
+      const y2 = path2[i].y;
+      path.push({
+        x: path1[i].x,
+        y: y1 + Math.sin((itt%frames)/frames * HPI)*(y2-y1)
+      });
+    }
+    drawPathDots(ctx, path, dotSize);
   }
 
   return {
@@ -45,13 +72,13 @@ export function getScene1(ctx, width, height) {
 
 export function getScene2(ctx, width, height) {
   printInfo('making scene 2');
-  const narative = 'What if the numbers have memory?';
+  const narative = '';
 
   ctx.strokeStyle = GRAY;
   ctx.fillStyle = WHITE;
   ctx.lineWidth = LINEWIDTH;
 
-  const edgeX = width*20/100;
+  const edgeX = width*5/100;
   const edgeY = height*20/100;
 
   const xMin = edgeX;
@@ -61,19 +88,22 @@ export function getScene2(ctx, width, height) {
   const yMid = (yMax+yMin)*0.5;
 
   const num = Math.floor(width / 10);
+  const dotSize = 3;
 
-  const noise = 2;
+  const noise = 1;
 
   let path = getLinspaceYLinspaceX(num, xMin, xMax, yMid, yMid);
+  let itt = 0;
 
   function scene() {
+    itt += 1;
     printInfo('scene2');
-    ctx.beginPath();
-    ctx.rect(0, 0, width, height);
-    ctx.fill();
+    ctx.fillStyle = WHITE;
+    clear(ctx, width, height);
+    ctx.fillStyle = GRAY;
 
     path = permuteY(path, noise);
-    drawPath(ctx, path);
+    drawPathDots(ctx, path, dotSize);
   }
 
   return {
